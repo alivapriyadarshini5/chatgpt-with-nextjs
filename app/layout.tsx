8b6cdae735655661b9ad5192b6dcd65ccc61e36d
ALIA <alivamallick9@gmail.com>
2023-05-27 15:33:15 +0530

    required package installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='flex'>
+        {/*Sidebar*/}
+
+        {/*ClientProvider-Notification*/}
+
+        <div className='bg-[#343541] flex-1'>{children}</div>
+        </div></body>
     </html>
   )
 }
